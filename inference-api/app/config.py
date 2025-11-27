c3ce9e66a577806d2202ce00516fd612687ad070
joao-miziaras <joao.miziara.emp@gmail.com>
2025-11-27 16:59:27 -0300

    Fix typo in AWS_SECRET_ACCESS_KEY variable name in config.py

--- inference-api/app/config.py
+++ inference-api/app/config.py
@@ -3,7 +3,7 @@ import os
 class Settings:
     # Credenciais AWS
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCES_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
     AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
